266c9ca2e7f923a4032d64ac81e61fd77a8048fe
Sakada12 <96065606+Sakada12@users.noreply.github.com>
2022-01-03 16:01:25 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,15 @@
 import bottle
 
+@bottle.route("/")
+def init():
+    return bottle.static_file("index.html", root = "")
+@bottle.get("/static/<filename>")
+def getStaticFile(filename):
+    return bottle.static_file(filename, root = "")
+@bottle.post("/static/<filename>")
+def getStaticFile(filename):
+    return bottle.static_file(filename, root = "")
+
 
     
 bottle.run(bottle.app(), port=8080, debug= True, reloader=True)
-init()
